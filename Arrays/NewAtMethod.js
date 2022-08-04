3ae7ba5551dcaa9eea0f7fa2f81ed50317f909d6
Maryna Markova <markova.maryna@gmail.com>
2022-08-04 15:45:53 +0300

    create forEach Loop examples

--- Arrays/NewAtMethod.js
+++ Arrays/NewAtMethod.js
@@ -10,9 +10,9 @@ console.log(arr[arr.length - 1]); //64
 // he same as:
 console.log(arr.slice(-1)[0]); //64
 // new at method:
-console.log(arr.ar(-2)); //64
+console.log(arr.at(-2)); //64
 
 // method at is working also with a string:
 
-console.log("maryna".at(0));
-console.log("maryna".at(-1));
+console.log("maryna".at(0)); //m
+console.log("maryna".at(-1)); //a
